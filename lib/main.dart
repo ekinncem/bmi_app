30c335bf02839271b1a8279898e3c7c04a3967b9
ekinncem <ekinn.ciem16@gmail.com>
2024-12-27 23:58:07 +0300

    new

--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,7 @@ class BMICalculator extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData(
+      theme:ThemeData(
         primaryColor: Color(0xFF0A0E21),
         colorScheme: ColorScheme.fromSwatch(),
         hintColor: Colors.purple,
@@ -29,23 +29,11 @@ class _InputPageState extends State<InputPage> {
         title: Text('BMI CALCULATOR'),
       ),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text('Body Text'),
-            SizedBox(height: 20.0),
-            ElevatedButton(
-              child: Text('Press Me'),
-              onPressed: () {
-                print('Raised Button Pressed');
-              },
-            ),
-          ],
-        ),
+        child: Text('Body Text'),
       ),
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.add),
-        onPressed: () {
+        onPressed: (){
           print('Floating Action Button Pressed');
         },
       ),
