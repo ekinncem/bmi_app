d76f4065d30602c43741c185d65b08ff73cdc656
ekinncem <ekinn.ciem16@gmail.com>
2024-12-29 23:57:01 +0300

    new

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -10,12 +10,43 @@ class _InputPageState extends State<InputPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('BMI CALCULATOR' , style: TextStyle(fontSize: 25 , color: Colors.white),),
+        title: Text(
+          'BMI CALCULATOR',
+          style: TextStyle(fontSize: 25, color: Colors.white),
+        ),
         centerTitle: true,
-        backgroundColor: Color(0xFF0A0E21)
+        backgroundColor: Color(0xFF0A0E21),
       ),
-      body: Center(
-        child: Text('Body Text'),
+      body: Row(
+        children: <Widget>[
+          Expanded(
+            child: Container(
+              margin: EdgeInsets.all(15),
+              decoration: BoxDecoration(
+                color: Color(0xFF1D1E33),
+                borderRadius: BorderRadius.circular(10),
+              ),
+            ),
+          ),
+          Expanded(
+            child: Container(
+              margin: EdgeInsets.all(15),
+              decoration: BoxDecoration(
+                color: Color(0xFF1D1E33),
+                borderRadius: BorderRadius.circular(10),
+              ),
+            ),
+          ),
+          Expanded(
+            child: Container(
+              margin: EdgeInsets.all(15),
+              decoration: BoxDecoration(
+                color: Color(0xFF1D1E33),
+                borderRadius: BorderRadius.circular(10),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
