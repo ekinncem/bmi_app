69252ff77eca6ecaca121bc2afa7524eaf4b3f1b
ekinncem <ekinn.ciem16@gmail.com>
2024-12-29 22:38:00 +0300

    new

--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,7 @@ class BMICalculator extends StatelessWidget {
       theme:ThemeData(
         primaryColor: Color(0xFF0A0E21),
         colorScheme: ColorScheme.fromSwatch(),
+        scaffoldBackgroundColor: Color(0xFF0A0E21),
         hintColor: Colors.purple,
       ),
       home: InputPage(),
