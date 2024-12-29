eecdb5fa616cde8972e4bde1230e5ceb1862cf8f
ekinncem <ekinn.ciem16@gmail.com>
2024-12-30 00:47:53 +0300

    new

--- lib/reusables.dart
+++ lib/reusables.dart
@@ -1,9 +1,6 @@
 import 'package:flutter/material.dart';
 
 class ReusableCard extends StatelessWidget {
-  const ReusableCard({
-    super.key,
-  });
 
   @override
   Widget build(BuildContext context) {
