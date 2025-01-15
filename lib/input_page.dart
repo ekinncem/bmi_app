9841f064674f918eb49162159f0cadba8687e3ca
ekinncem <ekinn.ciem16@gmail.com>
2025-01-15 23:57:41 +0300

    new

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -74,7 +74,7 @@ class _AgeCounterState extends State<AgeCounter> {
               color: Colors.white,
               iconSize: 36,
             ),
-            const SizedBox(width: 20),
+            const SizedBox(width: 7),
             IconButton(
               onPressed: _incrementAge,
               icon: const Icon(Icons.add),
