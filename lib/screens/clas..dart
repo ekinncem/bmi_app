055cbb976785a7f5d45db6861b348848fe96c479
ekinncem <ekinn.ciem16@gmail.com>
2025-01-17 23:16:35 +0300

    new

---
+++ lib/screens/clas..dart
@@ -0,0 +1,34 @@
+class HeightCounter extends StatefulWidget{
+  @override
+  _HeightCounterState createState() => _HeightCounterState();
+}
+
+class _HeightCounterState extends State<HeightCounter> {
+  int height = 120;
+
+  void _incrementHeight() {
+    setState(() {
+      height++;
+    });
+  }
+
+  void _decrementAge() {
+    setState((){
+      height--;
+    });
+  }
+
+  @override
+
+
+
+
+
+
+
+
+
+
+
+
+}
